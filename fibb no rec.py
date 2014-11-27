c59669ec923c968f7694ade02f453acce138cf63
gaassassins <savoreux69@gmail.com>
2014-11-27 19:37:57 +0300

    py

---
+++ fibb no rec.py
@@ -0,0 +1,13 @@
+def fibb(n): 
+
+	fib1 = 1
+	fib2 = 1
+	i = 2
+
+	while i < n:
+		fibs = fib1 + fib2
+		fib1 = fib2
+		fib2 = fibfinish
+		i = i + 1
+
+	return fibfinish;
---
+++ fibb rec.py
@@ -0,0 +1,6 @@
+def fibb(n): 
+
+if n == 1 or n == 2:
+	return 1
+else:
+return fibb(n-1) + fibb(n-2);
